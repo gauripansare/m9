3de17a4660966498a51e0d680a09a1f9da635969
atul0030 <atul@knowdl.com>
2018-11-01 15:03:50 +0530

    update hint events.js

--- scripts/events.js
+++ scripts/events.js
@@ -118,7 +118,7 @@ $(document).on("click", ".btnretry", function (event) {
 
 
 $(document).on("click", ".hintlink", function (event) {
-   var open = "open;"
+    var open = "open;"
     if ($(this).hasClass("expanded")) {
         $(".hintlink").removeClass("expanded")
         $(".hintlink").attr("aria-expanded", "false")
@@ -130,20 +130,20 @@ $(document).on("click", ".hintlink", function (event) {
         $(".hintcontainer").slideDown(100, function () {
             $(".hintlink").addClass("expanded");
             $(".hintlink").attr("aria-expanded", "true");
-            $(".hintcontainer .hintcontent").find("p:first").attr("tabindex","-1")
+            $(".hintcontainer .hintcontent").find("p:first").attr("tabindex", "-1")
             $(".hintcontainer .hintcontent").find("p:first").focus();
         });
     }
     if (_Navigator.IsRevel()) {
         LifeCycleEvents.OnInteraction("Hint button click. Hint " + open)
     }
-   
+
 });
 $(document).on("click", ".closehintlink", function (event) {
 
     $(".hintlink").removeClass("expanded")
     $(".hintlink").attr("aria-expanded", "false")
-    $(".hintcontainer").slideUp(100,function(){$("h2.pageheading").focus();});
+    $(".hintcontainer").slideUp(100, function () { $("h2.pageheading").focus(); });
     if (_Navigator.IsRevel()) {
         LifeCycleEvents.OnInteraction("Hint button click. Hint closed")
     }
@@ -204,31 +204,31 @@ $(document).on('mouseleave', ".hintlink", function (event) {
 });
 
 $(document).on("change", ".assessmentradio", function (event) {
-    if($(this).hasClass("disabled"))
-    return;
-    $(".assessmentSubmit").k_enable();    
+    if ($(this).hasClass("disabled"))
+        return;
+    $(".assessmentSubmit").k_enable();
 });
-function mouseenter(){
+function mouseenter() {
     $(".hintlink .hintlinkspan").css({ "color": "#b22222", "border-bottom": "1px solid #b22222" })
-    $(this).find("path").css({ "fill": "#b22222" })    
+    $(".hintlink").find("path").css({ "fill": "#b22222" })
 }
-function mouseleave(){
+function mouseleave() {
     $(".hintlink .hintlinkspan").css({ "color": "#047a9c", "border-bottom": "1px solid #047a9c" })
-    $(this).find("path").css({ "fill": "#047a9c" })
+    $(".hintlink").find("path").css({ "fill": "#047a9c" })
 }
 
 $(document).on("click", ".assessmentSubmit", function (event) {
     if (_Navigator.IsRevel()) {
         LifeCycleEvents.OnSubmit();
     }
-    gRecordData.Questions[currentQuestionIndex].UserSelectedOptionId = $("input[type='radio']:checked").attr("id") ;
+    gRecordData.Questions[currentQuestionIndex].UserSelectedOptionId = $("input[type='radio']:checked").attr("id");
     gRecordData.Questions[currentQuestionIndex].IsAnswered = true;
     _Navigator.GetBookmarkData();
     _Navigator.Next();
 });
 $(document).on('click', ".inputcircle", function (event) {
-   
-    $(this).closest("div").find(".inpputtext").trigger( "click" );
+
+    $(this).closest("div").find(".inpputtext").trigger("click");
 });
 
 window.onload = function () {
